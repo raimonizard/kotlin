9af9f7b1da62e8433e20c2d84d34afa7b6e4f605
= <=>
2023-12-15 16:12:27 +0100

    chore: first commit

---
+++ src/main/kotlin/ConsoleColors.kt
@@ -0,0 +1,74 @@
+package org.example
+
+// Reset
+const val RESET: String = "\u001b[0m" // Text Reset
+
+// Regular Colors
+const val BLACK: String = "\u001b[0;30m" // BLACK
+const val RED: String = "\u001b[0;31m" // RED
+const val GREEN: String = "\u001b[0;32m" // GREEN
+const val YELLOW: String = "\u001b[0;33m" // YELLOW
+const val BLUE: String = "\u001b[0;34m" // BLUE
+const val PURPLE: String = "\u001b[0;35m" // PURPLE
+const val CYAN: String = "\u001b[0;36m" // CYAN
+const val WHITE: String = "\u001b[0;37m" // WHITE
+
+// Bold
+const val BLACK_BOLD: String = "\u001b[1;30m" // BLACK
+const val RED_BOLD: String = "\u001b[1;31m" // RED
+const val GREEN_BOLD: String = "\u001b[1;32m" // GREEN
+const val YELLOW_BOLD: String = "\u001b[1;33m" // YELLOW
+const val BLUE_BOLD: String = "\u001b[1;34m" // BLUE
+const val PURPLE_BOLD: String = "\u001b[1;35m" // PURPLE
+const val CYAN_BOLD: String = "\u001b[1;36m" // CYAN
+const val WHITE_BOLD: String = "\u001b[1;37m" // WHITE
+
+// Underline
+const val BLACK_UNDERLINED: String = "\u001b[4;30m" // BLACK
+const val RED_UNDERLINED: String = "\u001b[4;31m" // RED
+const val GREEN_UNDERLINED: String = "\u001b[4;32m" // GREEN
+const val YELLOW_UNDERLINED: String = "\u001b[4;33m" // YELLOW
+const val BLUE_UNDERLINED: String = "\u001b[4;34m" // BLUE
+const val PURPLE_UNDERLINED: String = "\u001b[4;35m" // PURPLE
+const val CYAN_UNDERLINED: String = "\u001b[4;36m" // CYAN
+const val WHITE_UNDERLINED: String = "\u001b[4;37m" // WHITE
+
+// Background
+const val BLACK_BACKGROUND: String = "\u001b[40m" // BLACK
+const val RED_BACKGROUND: String = "\u001b[41m" // RED
+const val GREEN_BACKGROUND: String = "\u001b[42m" // GREEN
+const val YELLOW_BACKGROUND: String = "\u001b[43m" // YELLOW
+const val BLUE_BACKGROUND: String = "\u001b[44m" // BLUE
+const val PURPLE_BACKGROUND: String = "\u001b[45m" // PURPLE
+const val CYAN_BACKGROUND: String = "\u001b[46m" // CYAN
+const val WHITE_BACKGROUND: String = "\u001b[47m" // WHITE
+
+// High Intensity
+const val BLACK_BRIGHT: String = "\u001b[0;90m" // BLACK
+const val RED_BRIGHT: String = "\u001b[0;91m" // RED
+const val GREEN_BRIGHT: String = "\u001b[0;92m" // GREEN
+const val YELLOW_BRIGHT: String = "\u001b[0;93m" // YELLOW
+const val BLUE_BRIGHT: String = "\u001b[0;94m" // BLUE
+const val PURPLE_BRIGHT: String = "\u001b[0;95m" // PURPLE
+const val CYAN_BRIGHT: String = "\u001b[0;96m" // CYAN
+const val WHITE_BRIGHT: String = "\u001b[0;97m" // WHITE
+
+// Bold High Intensity
+const val BLACK_BOLD_BRIGHT: String = "\u001b[1;90m" // BLACK
+const val RED_BOLD_BRIGHT: String = "\u001b[1;91m" // RED
+const val GREEN_BOLD_BRIGHT: String = "\u001b[1;92m" // GREEN
+const val YELLOW_BOLD_BRIGHT: String = "\u001b[1;93m" // YELLOW
+const val BLUE_BOLD_BRIGHT: String = "\u001b[1;94m" // BLUE
+const val PURPLE_BOLD_BRIGHT: String = "\u001b[1;95m" // PURPLE
+const val CYAN_BOLD_BRIGHT: String = "\u001b[1;96m" // CYAN
+const val WHITE_BOLD_BRIGHT: String = "\u001b[1;97m" // WHITE
+
+// High Intensity backgrounds
+const val BLACK_BACKGROUND_BRIGHT: String = "\u001b[0;100m" // BLACK
+const val RED_BACKGROUND_BRIGHT: String = "\u001b[0;101m" // RED
+const val GREEN_BACKGROUND_BRIGHT: String = "\u001b[0;102m" // GREEN
+const val YELLOW_BACKGROUND_BRIGHT: String = "\u001b[0;103m" // YELLOW
+const val BLUE_BACKGROUND_BRIGHT: String = "\u001b[0;104m" // BLUE
+const val PURPLE_BACKGROUND_BRIGHT: String = "\u001b[0;105m" // PURPLE
+const val CYAN_BACKGROUND_BRIGHT: String = "\u001b[0;106m" // CYAN
+const val WHITE_BACKGROUND_BRIGHT: String = "\u001b[0;107m" // WHITE
---
+++ src/main/kotlin/Utilities.kt
@@ -0,0 +1,300 @@
+package org.example
+// TODO: Revisar powerpnt de M05-UF2 la ppt de dokka per afegir la captura del pas de les icones de recarregar l'arxiu build.gradle.kts
+
+import java.util.*
+
+val scan: Scanner = Scanner(System.`in`)
+
+/**
+ * This method can be used to read a String word value from the user through keyboard using java.util.Scanner
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @return outputValue Output value
+ */
+fun readWord(pMessageIn: String
+             , pMessageErrorDT: String
+): String{
+
+    var outputValue: String = ""
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNext()
+
+        if (!correctDataType){
+            println(RED + pMessageErrorDT + RESET)
+        }else{
+            outputValue = scan.next()
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read a String sentence from the user through keyboard using java.util.Scanner
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @return outputValue Output value
+ */
+fun readSentence(pMessageIn: String
+             , pMessageErrorDT: String
+): String{
+
+    var outputValue: String = ""
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNext()
+
+        if (!correctDataType){
+            println(RED + pMessageErrorDT + RESET)
+        }else{
+            outputValue = scan.nextLine()
+        }
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read a Char value from the user through keyboard using java.util.Scanner
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @return outputValue Output value
+ */
+fun readChar(pMessageIn: String
+             , pMessageErrorDT: String
+): Char{
+
+    var outputValue: Char = ' '
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNext()
+
+        if (!correctDataType){
+            println(RED + pMessageErrorDT + RESET)
+        }else{
+            // Get the first character of the input word from scan.next()
+            outputValue = scan.next()[0]
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read a Char value from the user through keyboard using java.util.Scanner. It can force the output Char to be lower or uppercase
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @param pCase Input parameter to force lowercase (0) or uppercase (1)
+ * @return outputValue Output value
+ */
+fun readChar(pMessageIn: String
+             , pMessageErrorDT: String
+             , pCase: Int
+): Char{
+
+    var outputValue: Char = ' '
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNext()
+
+        if (!correctDataType){
+            println(RED + pMessageErrorDT + RESET)
+        }else{
+            outputValue = if (pCase == 0) scan.next()[0].lowercaseChar()
+            else scan.next()[0].uppercaseChar()
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read a Boolean value from the user through keyboard using java.util.Scanner
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @return outputValue Output value
+ */
+fun readBoolean(pMessageIn: String
+                , pMessageErrorDT: String
+): Boolean{
+
+    var outputValue: Boolean = false
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNextBoolean()
+
+        if (!correctDataType){
+            println(RED + pMessageErrorDT + RESET)
+        }else{
+            outputValue = scan.nextBoolean()
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read an Int value from the user through keyboard using java.util.Scanner
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @return outputValue Output value
+ */
+fun readInt(pMessageIn: String
+            , pMessageErrorDT: String
+): Int{
+
+    var outputValue: Int = 0
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNextInt()
+
+        if (!correctDataType){
+            println(RED + pMessageErrorDT + RESET)
+        }else{
+            outputValue = scan.nextInt()
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read an Int value from the user through keyboard using java.util.Scanner in within a range
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @param pMessageErrorDV Data value error message to be shown to the user
+ * @param pMin Min accepted value
+ * @param pMax Max accepted value
+ * @return outputValue Output value
+ */
+fun readInt(pMessageIn: String
+            , pMessageErrorDT: String
+            , pMessageErrorDV: String
+            , pMin: Int
+            , pMax: Int
+): Int{
+
+    var outputValue: Int = 0
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNextInt()
+
+        if (!correctDataType){
+            println(RED + pMessageErrorDT + RESET)
+        }else{
+            outputValue = scan.nextInt()
+
+            if (outputValue < pMin || outputValue > pMax){
+                println(RED + pMessageErrorDV + RESET)
+                correctDataType = false
+            }
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read a Float value from the user through keyboard using java.util.Scanner
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @return outputValue Output value
+ */
+fun readFloat(pMessageIn: String
+              , pMessageErrorDT: String
+): Float{
+
+    var outputValue: Float = 0.0f
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNextFloat()
+
+        if (!correctDataType){
+            println(RED + pMessageErrorDT + RESET)
+        }else{
+            outputValue = scan.nextFloat()
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read a Float value from the user through keyboard using java.util.Scanner in within a range
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @param pMessageErrorDV Data value error message to be shown to the user
+ * @param pMin Min accepted value
+ * @param pMax Max accepted value
+ * @return outputValue Output value
+ */
+fun readFloat(pMessageIn: String
+            , pMessageErrorDT: String
+            , pMessageErrorDV: String
+            , pMin: Float
+            , pMax: Float
+): Float{
+
+    var outputValue: Float = 0.0f
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNextFloat()
+
+        if (!correctDataType){
+            println(RED + pMessageErrorDT + RESET)
+        }else{
+            outputValue = scan.nextFloat()
+
+            if (outputValue < pMin || outputValue > pMax){
+                println(RED + pMessageErrorDV + RESET)
+                correctDataType = false
+            }
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
